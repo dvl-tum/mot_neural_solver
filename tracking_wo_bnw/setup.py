9e0b42417a0243d7dc9c68e0f2296d0fb5f11b15
Guillem Braso Andilla <guillembrasoandilla@Guillems-MacBook-Pro.local>
2020-06-11 15:31:16 +0200

    Added tracktor submodule

--- tracking_wo_bnw/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='tracktor',
-      packages=['tracktor'],
-      package_dir={'':'src'},
-      version='0.0.1',
-      install_requires=[],)
